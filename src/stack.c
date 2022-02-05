faf5a22bce90645f9b0e8597654ffbbe541a5033
khadievedem <khadiev.edem@gmail.com>
2022-02-05 21:49:50 +0300

    Make format

--- src/stack.c
+++ src/stack.c
@@ -1,54 +1,61 @@
 #include "stack.h"
 
-stack_t *create_stack(void) {
-  stack_t *stack = malloc(sizeof *stack);
-  if (stack) {
-    stack->head = NULL;
-    stack->stackSize = 0;
-  }
-  return stack;
+stack_t* create_stack(void)
+{
+    stack_t* stack = malloc(sizeof *stack);
+    if (stack) {
+        stack->head = NULL;
+        stack->stackSize = 0;
+    }
+    return stack;
 }
 
-string_t copy_string_t(const string_t str) {
-  string_t tmp = malloc(strlen(str) + 1);
-  if (tmp)
-    strcpy(tmp, str);
-  return tmp;
+string_t copy_string_t(const string_t str)
+{
+    string_t tmp = malloc(strlen(str) + 1);
+    if (tmp)
+        strcpy(tmp, str);
+    return tmp;
 }
 
-void push(stack_t *stack, string_t value) {
-  stack_entry *entry = malloc(sizeof *entry);
-  if (entry) {
-    entry->data = copy_string(value);
-    entry->next = stack->head;
-    stack->head = entry;
-    stack->stackSize++;
-  }
+void push(stack_t* stack, string_t value)
+{
+    stack_entry* entry = malloc(sizeof *entry);
+    if (entry) {
+        entry->data = copy_string(value);
+        entry->next = stack->head;
+        stack->head = entry;
+        stack->stackSize++;
+    }
 }
 
-string_t top(stack_t *stack) {
-  if (stack && stack->head)
-    return stack->head->data;
-  else
-    return NULL;
+string_t top(stack_t* stack)
+{
+    if (stack && stack->head)
+        return stack->head->data;
+    else
+        return NULL;
 }
 
-string_t pop(stack_t *stack) {
-  string_t top_element = top(stack);
-  if (stack->head != NULL) {
-    stack->head = stack->head->next;
-    stack->stackSize--;
-  }
-  return top_element;
+string_t pop(stack_t* stack)
+{
+    string_t top_element = top(stack);
+    if (stack->head != NULL) {
+        stack->head = stack->head->next;
+        stack->stackSize--;
+    }
+    return top_element;
 }
 
-void clear(stack_t *stack) {
-  while (stack->head != NULL)
-    pop(stack);
+void clear(stack_t* stack)
+{
+    while (stack->head != NULL)
+        pop(stack);
 }
 
-void delete_stack(stack_t **stack) {
-  clear(*stack);
-  free(*stack);
-  *stack = NULL;
+void delete_stack(stack_t** stack)
+{
+    clear(*stack);
+    free(*stack);
+    *stack = NULL;
 }
