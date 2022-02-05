72ff700fe7c1d5c49d4b164a2880328015bbab76
khadievedem <khadiev.edem@gmail.com>
2022-02-05 12:03:25 +0300

    Make format

--- src/calculator.c
+++ src/calculator.c
@@ -18,14 +18,14 @@ int main(void)
     for (size_t counter = 0; fgets(line, 256, stdin); counter++) {
         if (counter == 0) {
             strcpy(expression, line);
-        }
-        else {
-            char *token = strtok(line, " ");
-            for (size_t i = 0; token; i++ ) {
-                if (i == 0) strcpy(variables[counter-1].name, token);
+        } else {
+            char* token = strtok(line, " ");
+            for (size_t i = 0; token; i++) {
+                if (i == 0)
+                    strcpy(variables[counter - 1].name, token);
                 else if (i == 2) {
-                    char *endptr;
-                    variables[counter-1].value = strtod(token, &endptr);
+                    char* endptr;
+                    variables[counter - 1].value = strtod(token, &endptr);
                 }
                 token = strtok(NULL, " ");
             }
