817dde0aa84ef866f8eb5de5d92ec36908f40374
khadievedem <khadiev.edem@gmail.com>
2022-02-05 19:07:16 +0300

    Added constants Added user-friendly text Code optimisation
    Removed unnecessary for loops
    Removed unnecessary if statements
    Removed magic numbers

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,12 @@
 #pragma once
+
 #include "variable.h"
 
-static char** split_to_tokens(char* infix_expr);
-char** to_rpn(char** infix_expr);
-char** variables_to_values(char** tokens);
+#define MAX_LENGTH 256
+#define MAX_VARS_AMOUNT 128
+
+// static char** split_to_tokens(char* infix_expr);
+//
+// char** to_rpn(char** infix_expr);
+//
+// char** variables_to_values(char** tokens);
