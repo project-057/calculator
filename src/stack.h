908cddabf56e6e7636f36146ae24b647c6dc0d50
khadievedem <khadiev.edem@gmail.com>
2022-02-05 20:57:02 +0300

    Format code

--- src/stack.h
+++ src/stack.h
@@ -2,28 +2,27 @@
 #include <string.h>
 
 typedef struct stack_entry {
-    char *data;
-    struct stack_entry *next;
+    char* data;
+    struct stack_entry* next;
 } stack_entry;
 
-typedef struct stack_t
-{
-    stack_entry *head;
+typedef struct stack_t {
+    stack_entry* head;
     size_t stackSize;
 } stack_t;
 
-typedef char *string_t;
+typedef char* string_t;
 
-stack_t *create_stack(void);
+stack_t* create_stack(void);
 
 string_t copy_string(const string_t);
 
-void push(stack_t *, string_t);
+void push(stack_t*, string_t);
 
-string_t top(stack_t *);
+string_t top(stack_t*);
 
-string_t pop(stack_t *);
+string_t pop(stack_t*);
 
-void clear(stack_t *);
+void clear(stack_t*);
 
-void delete_stack(stack_t **);
+void delete_stack(stack_t**);
