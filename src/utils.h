fdc88682e6551d979eca17511563908fbe6ed285
khadievedem <khadiev.edem@gmail.com>
2022-02-06 09:27:52 +0300

    Added static functions from utils.c

--- src/utils.h
+++ src/utils.h
@@ -9,10 +9,14 @@
 #define MAX_LENGTH 256
 #define MAX_VARS_AMOUNT 128
 
-// static char** split_to_tokens(char* infix_expr);
+// static char** split_to_tokens(char*);
 //
-bool is_double(char* str);
+static bool is_binary(char);
 //
-char** to_rpn(char** infix_expr);
+static int operations_priority(char, char);
 //
-// char** variables_to_values(char** tokens);
+bool is_double(char*);
+//
+char** to_rpn(char**);
+//
+// char** variables_to_values(char**);
