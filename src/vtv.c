fea6a0b2b939f7f2d7cd16bcd2560ba9d2e4d19b
Alexei Talanov <alexei.talanov003@mail.ru>
2022-02-07 11:58:22 +0300

    malloc and output changes

--- src/vtv.c
+++ src/vtv.c
@@ -1,22 +1,22 @@
 #include <math.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "utils.h"
 
-char** variables_to_values(char** tokens, Variable* vars_array, int token_size, int var_size)
+char** variables_to_values(char** tokens, Variable* vars_array, int token_size, int var_size) 
 {
-    char** out = (char**)malloc(sizeof(char*) * token_size);
+    char** out = malloc(sizeof(**out) * token_size);
     for (size_t i = 0; i < token_size; i++) {
-        out[i] = (char*)malloc(MAX_LENGTH);
+        out[i] = malloc(MAX_LENGTH);
         strcpy(out[i], tokens[i]);
     }
     for (size_t i = 0; i < token_size; i++) {
         if ((tokens[i][0] >= 'a' && tokens[i][0] <= 'z') || (tokens[i][0] >= 'A' && tokens[i][0] <= 'Z')) {
             for (size_t j = 0; j < var_size; j++) {
                 if (strcmp(tokens[i], vars_array[j].name) == 0) {
-                    sprintf(out[i], "%f", vars_array[j].value);
+                    sprintf(out[i], "%.9f", vars_array[j].value);
                     break;
                 }
             }
