027dea8e2d7f72c1625ed3a82307723c65303b15
khadievedem <khadiev.edem@gmail.com>
2022-02-08 18:39:52 +0300

    move inclusion of std. libraries form calculator to utils

--- src/calculator.c
+++ src/calculator.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "utils.h"
 #include "variable.h"
--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "variable.h"
 
