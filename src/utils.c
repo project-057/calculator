73bee4913a3c58af101a2d598b184f4448f0294e
Kutuzov Dmitry <archie-woc@ya.ru>
2022-02-08 21:58:51 +0300

    Adding declaration of TokenArrayFunctions

--- src/utils.c
+++ src/utils.c
@@ -8,3 +8,21 @@ bool is_double(String str)
     }
     return true;
 }
+
+TokenArray NewTokenArray() 
+{
+    TokenArray ta;
+    ta.array_ = malloc(sizeof(String)*MAX_LENGTH);
+    ta.size = 0;
+    return ta;
+}
+
+void PushBackToken(TokenArray* ta, String token)
+{
+    ta->array_[ta->size++] = token;
+}
+
+String TA_Pop(TokenArray* ta)
+{
+    return ta->array_[ta->size--];
+}
