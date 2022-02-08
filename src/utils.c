f3a8d317df33df7cef5cb57684d1be8020cff40c
khadievedem <khadiev.edem@gmail.com>
2022-02-08 18:31:30 +0300

    make format

--- src/utils.c
+++ src/utils.c
@@ -22,27 +22,28 @@ bool is_double(char* str)
 
 bool is_function(char* str)
 {
-	char functions[16][MAX_LENGTH];
-	strcpy(functions[0], "cos");
-	strcpy(functions[1], "sin");
-	strcpy(functions[2], "tg");
-	strcpy(functions[3], "log");
-	strcpy(functions[4], "ln");
-	strcpy(functions[5], "sqrt");
-	strcpy(functions[6], "pow");
-	strcpy(functions[7], "abs");
-	strcpy(functions[8], "exp");
-	strcpy(functions[9], "exp");
-	strcpy(functions[10], "real");
-	strcpy(functions[11], "imag");
-	strcpy(functions[12], "mag");
-	strcpy(functions[13], "phase");
+    char functions[16][MAX_LENGTH];
+    strcpy(functions[0], "cos");
+    strcpy(functions[1], "sin");
+    strcpy(functions[2], "tg");
+    strcpy(functions[3], "log");
+    strcpy(functions[4], "ln");
+    strcpy(functions[5], "sqrt");
+    strcpy(functions[6], "pow");
+    strcpy(functions[7], "abs");
+    strcpy(functions[8], "exp");
+    strcpy(functions[9], "exp");
+    strcpy(functions[10], "real");
+    strcpy(functions[11], "imag");
+    strcpy(functions[12], "mag");
+    strcpy(functions[13], "phase");
 
-	for (size_t i = 0; i < 14; i++) {
-		if (!strcmp(functions[i], str)) return true; /* strcmp(..., ...) == 0 
-													  * if strings are equal 
-													  */
-	}
+    for (size_t i = 0; i < 14; i++) {
+        if (!strcmp(functions[i], str))
+            return true; /* strcmp(..., ...) == 0
+                          * if strings are equal
+                          */
+    }
 
-    return false; 
+    return false;
 }
