f5d8584fe2d7bb9967088dd28ad9575f81057832
Kutuzov Dmitry <archie-woc@ya.ru>
2022-02-08 21:16:50 +0300

    TokenArray definition

--- src/utils.h
+++ src/utils.h
@@ -5,13 +5,25 @@
 #include <stdlib.h>
 
 #include "variable.h"
+#include "string.h"
 
 #define MAX_LENGTH 256
 #define MAX_VARS_AMOUNT 128
 
+typedef struct {
+    String* array_;
+    int size;
+} TokenArray;
+
+TokenArray NewTokenArray();
+
+void PushBackToken(TokenArray* ta, String token);
+
+String TA_Pop(TokenArray* ta);
+
 // static char** split_to_tokens(char*);
 //
-bool is_double(char*);
+bool is_double(String value);
 // char** to_rpn(char**);
 //
 // char** variables_to_values(char**);
