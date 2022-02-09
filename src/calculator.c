af289f76ecb6ed300067580eab2faaece2398379
Kutuzov Dmitry <archie-woc@ya.ru>
2022-02-09 10:22:07 +0300

    Tested eval with TokenArray

--- src/calculator.c
+++ src/calculator.c
@@ -2,6 +2,7 @@
 
 #include "utils.h"
 #include "variable.h"
+#include "eval.h"
 
 const char* variable_request = "Write a variable with its value \033[0;33m(or press <C-d> to stop)\
                               \n\033[0;31muse format -> 'x = 1.0' :\033[0m";
@@ -19,6 +20,7 @@ int main(void)
 
     puts(variable_request);
 
+    /* TODO: Using get_variable instead */
     for (size_t counter = 0; fgets(line, MAX_LENGTH, stdin); counter++) {
         puts(variable_request);
         whitespace_cleaner(line);
@@ -33,6 +35,7 @@ int main(void)
         variables[counter].value = strtod(token, &endptr); /* add value to the Variable structure */
     }
 
+    /* Use TokenArray instead char** */
     // char **tokens = calloc(MAX_LENGTH, sizeof(char*));
     // for(size_t i = 0 ; i < 11 ; i++)
     // {
