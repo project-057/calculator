d936c36f934b67e5fd2631b4d532aec2f2d62270
Alexei Talanov <alexei.talanov003@mail.ru>
2022-02-11 16:58:47 +0300

    TokenArray addition + last master commit(s)

--- src/utils.h
+++ src/utils.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <stdio.h>
 
 #include "variable.h"
 
