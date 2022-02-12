c81568419a4c9cd95c1b2cb05fed420b0ed454df
Oskar Sharipov <oskarsh[at]riseup[dot]net>
2022-02-12 18:53:07 +0300

    Add test suit for whitespace_cleaner

--- tests/smoke.c
+++ tests/smoke.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "../src/utils.h"
 
 #include "greatest.h"
@@ -26,10 +28,27 @@ SUITE(is_double_suit)
 	RUN_TEST(is_double_shouldnt_mark_incorrect_doubles_test);
 }
 
+TEST whitespace_cleaner_should_remove_whitespaces(void)
+{
+	char arg[] = "1+ 2/3    *7"; whitespace_cleaner(arg);
+	ASSERT_STR_EQ(arg, "1+2/3*7");
+
+	char arg2[] = " "; whitespace_cleaner(arg2);
+	ASSERT_STR_EQ(arg2, "");
+
+	PASS();
+}
+
+SUITE(whitespace_cleaner_suit)
+{
+	RUN_TEST(whitespace_cleaner_should_remove_whitespaces);
+}
+
 GREATEST_MAIN_DEFS();
 int main(int argc, char **argv)
 {
 	GREATEST_MAIN_BEGIN();
 	RUN_SUITE(is_double_suit);
+	RUN_SUITE(whitespace_cleaner_suit);
 	GREATEST_MAIN_END();
 }
