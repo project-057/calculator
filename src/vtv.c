42d01cf02ea7c3ee465331705ad8fab16ed763ac
Kutuzov Dmitry <archie-woc@ya.ru>
2022-02-15 01:42:53 +0300

    Rewrite vtv

--- src/vtv.c
+++ src/vtv.c
@@ -6,26 +6,21 @@
 
 TokenArray variables_to_values(TokenArray tokens, Variable* vars_array, int var_size)
 {
-    char** out1 = (char**)malloc(sizeof(char*) * tokens.size);
+    TokenArray result = create_token_array();
     for (int i = 0; i < tokens.size; i++) {
-        out1[i] = (char*)malloc(256);
-        strcpy(out1[i], tokens.array[i]);
+        strcpy(result.array[i], tokens.array[i]);
     }
 
     for (int i = 0; i < tokens.size; i++) {
-        if ((tokens.array[i][0] >= 'a' && tokens.array[i][0] <= 'z') || (tokens.array[i][0] >= 'A' && tokens.array[i][0] <= 'Z')) {
+        if (isalpha(tokens.array[i][0])) {
             for (int j = 0; j < var_size; j++) {
                 if (strcmp(tokens.array[i], vars_array[j].name) == 0) {
-                    sprintf(out1[i], "%f", vars_array[j].value);
+                    sprintf(result.array[i], "%f", vars_array[j].value);
                     break;
                 }
             }
         }
     }
 
-	TokenArray result = {
-		.array = out1,
-		.size = tokens.size
-	};
     return result;
 }
