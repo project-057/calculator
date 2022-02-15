af4b09217e20a9be7e742c4ac6eb4a318796258e
Edem Khadiev <72758710+khadievedem@users.noreply.github.com>
2022-02-15 03:03:29 +0300

    Fix vtv wtf (#31)
    * * Rewrite message for get variable
    
    * * Make main
    
    * * Oskarification
    
    * * repair vtv function
    
    * Format code
    
    Co-authored-by: Oskar Sharipov <oskarsh[at]riseup[dot]net>

--- src/calculator.c
+++ src/calculator.c
@@ -19,11 +19,14 @@ int main(void)
     while (variables[var_size++].name[0]) { }
 
     TokenArray new_infix_expr = variables_to_values(infix_expr, variables, var_size - 1);
+    free_token_array(&infix_expr);
 
     TokenArray postfix_expr = to_rpn(new_infix_expr);
+    free_token_array(&new_infix_expr);
 
     double result = eval(postfix_expr);
-
     printf("Result: \e[1;32m%f\033[0m\n", result);
+
+    free_token_array(&postfix_expr);
     return 0;
 }
